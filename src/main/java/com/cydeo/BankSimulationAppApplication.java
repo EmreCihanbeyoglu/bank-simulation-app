eb7203c0ed936bb4c3cd3c8277f8b8f2bc395502
Emre Cihanbeyoglu <mrcihanbeyoglu@gmail.com>
2024-09-27 14:02:43 +0200

    add security implementation

--- src/main/java/com/cydeo/BankSimulationAppApplication.java
+++ src/main/java/com/cydeo/BankSimulationAppApplication.java
@@ -9,6 +9,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.beans.BeanProperty;
 import java.math.BigDecimal;
@@ -37,5 +39,6 @@ public class BankSimulationAppApplication {
     public ModelMapper getModelMapper() {
         return new ModelMapper();
     }
+
 
 }
---
+++ src/main/java/com/cydeo/service/impl/SecurityServiceImpl.java
@@ -0,0 +1,27 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.entity.UserPrincipal;
+import com.cydeo.entity.User;
+import com.cydeo.repository.UserRepository;
+import com.cydeo.service.SecurityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityServiceImpl implements SecurityService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public SecurityServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username));
+        return new UserPrincipal(user);
+    }
+}
